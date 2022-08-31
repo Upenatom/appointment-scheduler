92d6a924fa46a64675a64fcefdd557179e15f793
Randolph <randolph@Randolphs-Mini.home>
2022-08-31 10:33:10 -0500

    admin page displaying all apointments

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,17 +3,22 @@ const Car = require("../models/car");
 module.exports = { index };
 
 function index(req, res) {
-  console.log("you've hit admin index contorller");
   Customer.findById(req.user.id, function (err, admin) {
-    // console.log(admin);
     if (admin.isAdmin) {
-      Car.find({}, function (err, car) {
-        Customer.find({}, function (err, customer) {
-          res.render("admin/index", { car, customer });
+      Car.find({})
+        .populate("customer")
+        .exec(function (err, car) {
+          Customer.find({}, function (err, customer) {
+            res.render("admin/index", { car, customer });
+          });
         });
-      });
+
+      //   Car.find({}, function (err, car) {
+      //     Customer.find({}, function (err, customer) {
+      //       res.render("admin/index", { car, customer });
+      //     });
+      //   });
     } else {
-      console.log("you've hit homepage render");
       res.redirect("/");
     }
   });
